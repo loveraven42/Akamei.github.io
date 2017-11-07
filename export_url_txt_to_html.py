178d368f879704a1f46a33713b91e61e2a3c3af9
Akame <1246165945@qq.com>
2017-11-07 09:38:51 +0800

    Update export_url_txt_to_html.py

--- export_url_txt_to_html.py
+++ export_url_txt_to_html.py
@@ -1,9 +1,19 @@
+import requests
+import re
+
 f = open("urls.txt", "r")
 lines = f.readlines()
-f1 = open("urls.html", "w")
+pattern = "<title>(.*?)</title>"
+
+f = open("urls.html", "w")
+
 for line in lines:
     line = line.strip("\n")
-    f1.write("<a href='"+line+"'>" + line + "</a>")
-    f1.write("<br/>")
-f.close()
-f1.close()
+    r = requests.get(line)
+    code = r.status_code
+    title = re.findall(pattern=pattern, r.content)
+    if r.headers['server']:
+        server = r.headers['server']
+    else:
+        server = ""
+    f.write("[" + code + "] " + server  + "  " + title + "<a href='" + line + "'>" + line + "</a>\n" )
