6c33bb0dfea9ba6545c75fcccd2ebf1a174c7427
Akame <1246165945@qq.com>
2017-12-25 18:45:01 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.js"></script>
-<script>
-var data = {"content":"123","styleid":"1","from":"share","sourceUrl":"","appKey":"8003029170","source":"","refer":"","url_type":"","object_type":"","visible":"0","pic_ids":"","gid":""}
-$.ajax({
-  url: "https://service.weibo.com/share/aj_share.php",
-  type: "POST",
-  data: JSON.stringify(data),
-  success: function(a) {
-    alert(a);
-  }
-});
-</script>
+// <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.js"></script>
+// <script>
+// var data = {"content":"123","styleid":"1","from":"share","sourceUrl":"","appKey":"8003029170","source":"","refer":"","url_type":"","object_type":"","visible":"0","pic_ids":"","gid":""}
+// $.ajax({
+//   url: "https://service.weibo.com/share/aj_share.php",
+//   type: "POST",
+//   data: JSON.stringify(data),
+//   success: function(a) {
+//     alert(a);
+//   }
+// });
+// </script>
+<script>alert(1)</script>
