589bd36a3b529794f2d288edd07daf4621048403
Akame <1246165945@qq.com>
2017-12-25 23:32:10 +0800

    Update test.js

--- test.js
+++ test.js
@@ -15,10 +15,11 @@ if(window.XMLHttpRequest){
 }
 //设置请求的类型及url
 //post请求一定要添加请求头才行不然会报错
-xhr.open('post', 'https://service.weibo.com/share/aj_share.php' );
+alert(1);
+xhr.open('post', 'http://weibo.com/aj/mblog/add?ajwvr=6&__rnd=1514215803895' );
 xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 //发送请求
-xhr.send('content=123%20%20&styleid=1&from=share&sourceUrl=&appkey=8003029170&source=&refer=&url_type=&object_type=&visible=0&pic_ids=&gid=');
+xhr.send('location=v6_content_home&text=13&appkey=&style_type=1&pic_id=&tid=&pdetail=&rank=0&rankid=&module=stissue&pub_source=main_&pub_type=dialog&isPri=0&_t=0');
 xhr.onreadystatechange = function () {
     // 这步为判断服务器是否正确响应
   if (xhr.readyState == 4 && xhr.status == 200) {
