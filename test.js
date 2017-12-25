dd2a96fc6fdd5c83b9d0b8c571be495949a65b8f
Akame <1246165945@qq.com>
2017-12-25 19:16:25 +0800

    Update test.js

--- test.js
+++ test.js
@@ -26,11 +26,12 @@ if(window.XMLHttpRequest){
 }else{
       console.log(e);
 }
-xhr.open('post', 'http://shop.sc.weibo.com/aj/h5/ship/add' );
+xhr.open('post', 'https://service.weibo.com/share/aj_share.php' );
 xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 //发送请求
 xhr.send('content=123%20%20&styleid=1&from=share&sourceUrl=&appkey=8003029170&source=&refer=&url_type=&object_type=&visible=0&pic_ids=&gid=');
 xhr.onreadystatechange = function () {
+    // 这步为判断服务器是否正确响应
   if (xhr.readyState == 4 && xhr.status == 200) {
     alert("ok");
     console.log(xhr.responseText);
