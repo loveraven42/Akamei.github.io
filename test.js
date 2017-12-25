68f91899406a033e2f7e47e86bc3f1c3b73b6cb1
Akame <1246165945@qq.com>
2017-12-25 23:50:30 +0800

    Update test.js

--- test.js
+++ test.js
@@ -16,10 +16,10 @@ if(window.XMLHttpRequest){
 //设置请求的类型及url
 //post请求一定要添加请求头才行不然会报错
 alert(1);
-xhr.open('post', 'http://weibo.com/aj/mblog/add?ajwvr=6&__rnd=1514215803895' );
+xhr.open('post', 'http://account.weibo.com/set/aj5/prefer/watermark' );
 xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 //发送请求
-xhr.send('location=v6_content_home&text=13&appkey=&style_type=1&pic_id=&tid=&pdetail=&rank=0&rankid=&module=stissue&pub_source=main_&pub_type=dialog&isPri=0&_t=0');
+xhr.send('wradio=1&picpostion=3&nickname=1&icon=1&address=0&_t=0');
 xhr.onreadystatechange = function () {
     // 这步为判断服务器是否正确响应
   if (xhr.readyState == 4 && xhr.status == 200) {
